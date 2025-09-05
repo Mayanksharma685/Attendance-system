68020f4035868516c00f954b23353ee2bc03030c
nitinn13 <nitinjha080@gmail.com>
2025-09-06 01:43:04 +0530

    completed auth && init frontend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,23 +1,18 @@
-import express, { type Request, type Response } from "express";
+import express, {} from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import authRouter from "./routes/auth.js";
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
-
-app.get("/test", (req: Request, res: Response) => {
-  res.send("Healthy server");
+app.get("/test", (req, res) => {
+    res.send("Healthy server");
 });
 app.use("/auth", authRouter);
-
-
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
-
+//# sourceMappingURL=index.js.map
