b8dbe8655f5b620a3c81d8ee4680d1dd551950d3
Mayank Sharma <sharmamay685@gmail.com>
2025-09-20 17:40:16 +0530

    qr backend

--- qr-backend/routes/qrRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { generateQR, verifyQR } from "../controllers/qrController.js";
-
-const router = express.Router();
-router.get("/generate-qr", generateQR);
-router.post("/verify-qr", verifyQR);
-
-export default router;
